6f28a60afabc95b9ca25c18296d5f7abb81cc58d
AlexRassyhin <alexrassyhin4@gmail.com>
2021-11-22 23:00:34 +0200

    add file

---
+++ Find me.py
@@ -0,0 +1,18 @@
+import random
+
+x=[random.randint(0,10) for i in range (10)]
+print(x)
+print('число 3 находится в списке под номером',x.index(3))
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Генераторы списков.py
+++ Генераторы списков.py
@@ -1,7 +1,6 @@
 import random
 
 x=int(input("enter value = "))
-x=[random.randint(0,9) for i in range (10)]
-
+x=[random.randint(0,10) for i in range (10)]
 print(x)
 
