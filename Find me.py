c7878cd3aa9af6b0db3fb95f2746487ba4752c8e
AlexRassyhin <alexrassyhin4@gmail.com>
2022-07-10 14:54:32 +0300

    add file

--- Find me.py
+++ Find me.py
@@ -2,7 +2,7 @@ import random
 
 x=[random.randint(0,10) for i in range (10)]
 print(x)
-print('число 3 находится в списке под номером',x.index(3))
+print('число 3 находится в списке под номером',x.index(3)+1)
 
 
 
